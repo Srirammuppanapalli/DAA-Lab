706d81e5f783124371141ff8ae83b57378c5cc18
Srirammuppanapalli <11249a237@kanchiuniv.ac.in>
2026-04-24 22:00:48 +0530

    Update Best fit.c

--- Best fit.c
+++ Best fit.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
-
 void bestFit(int items[], int n, int capacity)
 {
     printf("\nBest Fit Algorithm\n");
-
     int bin[n];
     int binCount = 0;
-
-    // Initialize bins with full capacity
     for (int i = 0; i < n; i++)
         bin[i] = capacity;
-
-    // Process each item
     for (int i = 0; i < n; i++)
     {
         int bestIndex = -1;
         int minSpace = capacity + 1;
-
-        // Find best bin
         for (int j = 0; j < binCount; j++)
         {
             if (bin[j] >= items[i] && (bin[j] - items[i]) < minSpace)
@@ -26,7 +18,6 @@ void bestFit(int items[], int n, int capacity)
                 minSpace = bin[j] - items[i];
             }
         }
-
         if (bestIndex != -1)
         {
             bin[bestIndex] -= items[i];
@@ -39,28 +30,21 @@ void bestFit(int items[], int n, int capacity)
             binCount++;
         }
     }
-
     printf("Total bins used = %d\n", binCount);
 }
-
 int main()
 {
     int n, capacity,itemSize;
-
     printf("Enter number of items: ");
     scanf("%d", &n);
-
     printf("Enter bin capacity: ");
     scanf("%d", &capacity);
-
     int items[n];
-
     printf("Enter item sizes:\n");
     for (int i = 0; i < n; i++)
     {
          printf("Item %d: ", i + 1);
             scanf("%d", &itemSize);
-
             if (itemSize <= capacity)
             {
                 items[i] = itemSize;
@@ -72,6 +56,31 @@ int main()
         }
    
     bestFit(items, n, capacity);
-
     return 0;
 }
+
+
+Output:
+
+Enter number of items: 5
+Enter bin capacity: 7
+Enter item sizes:
+Item 1: 2
+Item 2: 3
+Item 3: 4
+Item 4: 5
+Item 5: 1
+
+Best Fit Algorithm
+Item 1 (weight: 2) placed in Bin 1
+Item 2 (weight: 3) placed in Bin 1
+Item 3 (weight: 4) placed in Bin 2
+Item 4 (weight: 5) placed in Bin 3
+Item 5 (weight: 1) placed in Bin 1
+Total bins used = 3
+
+
+
+
+
+
