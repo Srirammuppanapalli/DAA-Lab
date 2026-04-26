7d9959ff951bf51d267d7fd8ffd4327d3b06f0cf
Srirammuppanapalli <11249a237@kanchiuniv.ac.in>
2026-04-26 11:27:47 +0530

    Update TowerOfHanoi.c

--- TowerOfHanoi.c
+++ TowerOfHanoi.c
@@ -1,21 +1,41 @@
+Aim:
+To implement the Tower of Hanoi problem using recursion in C and display the sequence of moves required to transfer disks from source to destination.
+
+Program:
 #include <stdio.h>
+
+// Function to solve Tower of Hanoi
 void toh(int diskcount, char s, char d, char t){
-    if (diskcount==1)
+    
+    // Base case: if only one disk, move it directly
+    if (diskcount == 1)
     {
-        printf(" \n move disk %d from %c to %c",diskcount,s,d);
-    return;
+        printf("\nMove disk %d from %c to %c", diskcount, s, d);
+        return;
+    }
+
+    // Step 1: Move n-1 disks from source (s) to temporary (t)
+    toh(diskcount - 1, s, t, d);
+
+    // Step 2: Move the largest disk from source to destination
+    printf("\nMove disk %d from %c to %c", diskcount, s, d);
+
+    // Step 3: Move n-1 disks from temporary (t) to destination (d)
+    toh(diskcount - 1, t, d, s);
 }
- toh(diskcount-1,s,t,d);
- printf(" \n move disk %d from %c to %c",diskcount,s,d);
- toh(diskcount-1,t,d,s);
-}
+
 int main() {
     int n;
-    printf("enter number of disks: ");
-    scanf("%d",&n);
-    toh(n,'s','d','t');
+
+    // Input number of disks
+    printf("Enter number of disks: ");
+    scanf("%d, &n);
+
+    // Call Tower of Hanoi function
+    toh(n, 'S', 'D', 'T');  // S = Source, D = Destination, T = Temporary
+
     return 0;
-}    
+}
 Output:
 enter number of disks: 4
  
